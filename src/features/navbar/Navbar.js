42c25d7da078936ae42413da7ec266857d448300
Mayankmishra110 <kmmay15@gmail.com>
2024-05-31 16:52:19 +0530

    admin role, edit products feature added

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -8,15 +8,12 @@ import {
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectItems } from "../cart/cartSlice";
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
+import { selectLoggedInUser } from "../auth/authSlice";
+
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
+  { name: "Dashboard", link: "#", user: true },
+  { name: "Team", link: "#", user: true },
+  { name: "Admin", link: "/admin", admin: true },
 ];
 const userNavigation = [
   { name: "My Profile", link: "/profile" },
@@ -30,6 +27,7 @@ function classNames(...classes) {
 
 function NavBar({ children }) {
   const items = useSelector(selectItems);
+  const user = useSelector(selectLoggedInUser);
   return (
     <>
       <div className="min-h-full">
